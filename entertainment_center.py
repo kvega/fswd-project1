828fb2a61754b665410d051b8b4144c2a2c0dd0e
kvega <kevin.n.vega@gmail.com>
2018-03-24 16:08:44 -0700

    pep8 formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,45 +5,52 @@ import fresh_tomatoes
 
 # Create instances of favorite movies
 empire_strikes_back = media.Movie("Empire Strikes Back",
-        "Luke continues on his his to become a Jedi",
-        "https://t1.gstatic.com/images?q=tbn:ANd9GcTtXwQAEDxEY3E9Nn78H96VZCjlV6hZWPlDd5IpVNyeuzO2vT17",
-        "https://www.youtube.com/watch?v=JNwNXF9Y6kY")
+                                  "Luke continues on his his to become a Jedi",
+                                  "https://t1.gstatic.com/images?q=tbn:ANd9G" +
+                                  "cTtXwQAEDxEY3E9Nn78H96VZCjlV6hZWPlDd5IpVN" +
+                                  "yeuzO2vT17",
+                                  "https://www.youtube.com/watch?v=JNwNXF9Y6kY")
 
 
 blade_runner = media.Movie("Blade Runner",
-        "Description",
-        "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
-        "https://www.youtube.com/watch?v=JMyz9bnKITY")
+                           "Description",
+                           "https://upload.wikimedia.org/wikipedia/en/5/53/B" +
+                           "lade_Runner_poster.jpg",
+                           "https://www.youtube.com/watch?v=JMyz9bnKITY")
 
 
 gladiator = media.Movie("Gladiator",
-        "Description",
-        "https://t3.gstatic.com/images?q=tbn:ANd9GcRuhegCaGHfcQc-Owpib9vGSO0hUlSUMTojO2s4ih8oXqHBw2ks",
-        "https://www.youtube.com/watch?v=w5rVtxWTq8w")
+                        "Description",
+                        "https://t3.gstatic.com/images?q=tbn:ANd9GcRuhegCaGH" +
+                        "fcQc-Owpib9vGSO0hUlSUMTojO2s4ih8oXqHBw2ks",
+                        "https://www.youtube.com/watch?v=w5rVtxWTq8w")
 
 
 up = media.Movie("Up",
-        "Description",
-        "https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg",
-        "https://www.youtube.com/watch?v=pkqzFUhGPJg")
+                 "Description",
+                 "https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_" +
+                 "film%29.jpg",
+                 "https://www.youtube.com/watch?v=pkqzFUhGPJg")
 
 
 the_martian = media.Movie("Blade Runner",
-        "Description",
-        "https://t2.gstatic.com/images?q=tbn:ANd9GcTkKPZ7EIOafEsemyn6zTIDeGYthKC_Okgxi1eX6diuOT3xKWXQ",
-        "https://www.youtube.com/watch?v=ej3ioOneTy8")
+                          "Description",
+                          "https://t2.gstatic.com/images?q=tbn:ANd9GcTkKPZ7E" +
+                          "IOafEsemyn6zTIDeGYthKC_Okgxi1eX6diuOT3xKWXQ",
+                          "https://www.youtube.com/watch?v=ej3ioOneTy8")
 
 
 interstellar = media.Movie("Interstellar",
-        "Description",
-        "https://t1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB",
-        "https://www.youtube.com/watch?v=2LqzF5WauAw")
+                           "Description",
+                           "https://t1.gstatic.com/images?q=tbn:ANd9GcRf61mk" +
+                           "er2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB",
+                           "https://www.youtube.com/watch?v=2LqzF5WauAw")
 
 movies = [empire_strikes_back,
-        blade_runner,
-        gladiator,
-        up,
-        the_martian,
-        interstellar]
+          blade_runner,
+          gladiator,
+          up,
+          the_martian,
+          interstellar]
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,22 +2,23 @@
 
 import webbrowser
 
+
 class Movie():
 	"""Movie class
-
+	
 	Attributes:
 		title (str): the title of the movie
 		storyline (str): summary of the movie's plot
 		poster_image_url (str): link to image of movie's poster
 		trailer_youtue_url (str): link to movie's trailer on YouTube
 	"""
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
-        self.title = title
-        self.storyline = storyline
-        self.poster_image_url = poster_image_url 
-        self.trailer_youtube_url = trailer_youtube_url 
+	def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+		self.title = title
+		self.storyline = storyline
+		self.poster_image_url = poster_image_url 
+		self.trailer_youtube_url = trailer_youtube_url 
 
-    def show_trailer(self):
-    	"""Method to open movie's YouTube trailer
-    	"""
-        webbrowser.open(self.trailer_youtube_url)
+	def show_trailer(self):
+		"""Method to open movie's YouTube trailer
+		"""
+		webbrowser.open(self.trailer_youtube_url)
